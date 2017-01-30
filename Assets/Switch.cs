0c8eccd95663d4a9d093fe7b6e063b9998b80c7d
Jean-Philippe Deblonde <jean-philippe.deblonde@saint-gobain.com>
2017-01-30 14:19:08 +0100

    First commit

---
+++ Assets/Switch.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Switch : MonoBehaviour {
+
+    bool rgbMaterialOn = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void switchMaterial()
+    {
+        rgbMaterialOn = !rgbMaterialOn;
+
+        if (rgbMaterialOn)
+        {
+            Debug.Log("Enabling Keyword \"RGB_ON\"");
+            Shader.EnableKeyword("RGB_ON");
+            Shader.DisableKeyword("RGB_OFF");
+        }else
+        {
+            Debug.Log("Disabling Keyword \"RGB_ON\"");
+            Shader.EnableKeyword("RGB_OFF");
+            Shader.DisableKeyword("RGB_ON");
+        }
+    }
+
+}
